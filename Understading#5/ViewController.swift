4a2502ab83e21fb13c0c124e715ed3d8b5dbbaf5
nakanodesukun <94818124+nakanodesukun@users.noreply.github.com>
2021-12-24 13:39:58 +0900

    Initial Commit

---
+++ Understading#5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Understading#5
+//
+//  Created by 中野翔太 on 2021/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
